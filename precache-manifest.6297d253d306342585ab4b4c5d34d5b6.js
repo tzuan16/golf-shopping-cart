623f45cebbc1fdc86e2d1ef9d056b6dc220c3182
tzuan16 <hungan@uw.edu>
2020-09-07 10:22:36 +0800

    Updates

---
+++ precache-manifest.6297d253d306342585ab4b4c5d34d5b6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae9de94a70af13ddbf47938a11d5bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdaaf22d4a667071622d",
+    "url": "/static/css/2.6bd63359.chunk.css"
+  },
+  {
+    "revision": "c004354ec0e934ebe703",
+    "url": "/static/css/main.d19c3cd6.chunk.css"
+  },
+  {
+    "revision": "cdaaf22d4a667071622d",
+    "url": "/static/js/2.49e91ae0.chunk.js"
+  },
+  {
+    "revision": "dd15359b453a3ff19ff7cf9a7f74ec93",
+    "url": "/static/js/2.49e91ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c004354ec0e934ebe703",
+    "url": "/static/js/main.676a3779.chunk.js"
+  },
+  {
+    "revision": "563d0d19426e5177b878",
+    "url": "/static/js/runtime-main.7898ed8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
